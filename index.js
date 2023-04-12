8c6d019d90b2b8a4c789b3f210f6ce417ae34409
MikeMeerschaert <meerschaertm@callan.com>
2023-04-12 14:39:48 -0700

    created example of margin and valign issue

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ import PptxGenJS from "pptxgenjs"
 
 const presentation = new PptxGenJS()
 
-const slide = presentation.addSlide()
+presentation.defineSlideMaster({
+  title: "margin and valign issue demo",
+  background: { color: "FFFFFF" },
+  objects: [
+    {
+      placeholder: {
+        options: {
+          name: "title",
+          type: "body",
+          x: 0.5,
+          y: 0.1,
+          w: 9,
+          h: 1,
+          valign: "bottom",
+          margin: [0, 0, 0, 0],
+        },
+        text: "valign: bottom",
+      },
+    },
+  ],
+})
 
 presentation.writeFile({ fileName: "example.pptx" })
