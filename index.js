5c70dfb31c17126f1fe144a0aec3b404799e78aa
MikeMeerschaert <meerschaertm@callan.com>
2023-04-26 09:50:27 -0700

    added slide masters demonstrating issues with margin, vertical alignment and bullet point formatting

--- index.js
+++ index.js
@@ -18,7 +18,33 @@ presentation.defineSlideMaster({
           valign: "bottom",
           margin: [0, 0, 0, 0],
         },
-        text: "valign: bottom",
+        text: "valign: bottom; margin: 0",
+      },
+    },
+  ],
+})
+
+presentation.defineSlideMaster({
+  title: "Bullet point demo",
+  background: { color: "FFFFFF" },
+  objects: [
+    {
+      placeholder: {
+        options: {
+          name: "Bullets",
+          type: "body",
+          bullet: {
+            type: "bullet",
+            characterCode: "25CF",
+          },
+          x: 0.5,
+          y: 0.1,
+          w: 9,
+          h: 1,
+          fontSize: 12,
+          fontFace: "Arial",
+        },
+        text: "bullet 1\nbullet 2\nbullet 3",
       },
     },
   ],
